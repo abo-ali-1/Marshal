960c0d979397648bbf85fedd38354e522df337b9
Marshal <mmd127328@gmail.com>
2025-09-07 22:57:31 +0300

    Delete rm_gangs directory

--- rm_gangs/web/src/enums/events.ts
+++
@@ -1,7 +0,0 @@
-export enum Receive {
-	visible = 'setVisible',
-}
-
-export enum Send {
-	close = 'nuiClosed',
-}
--- rm_gangs/web/src/utils/debug/visibility.ts
+++
@@ -1,8 +0,0 @@
-import { Receive } from '@enums/events';
-import { DebugEventSend } from '@utils/eventsHandlers';
-/**
- * The debug response for the visibility debug action.
- */
-export function setVisible(visible: boolean): void {
-	DebugEventSend(Receive.visible, visible);
-}
